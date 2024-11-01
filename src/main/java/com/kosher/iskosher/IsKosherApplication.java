84986cc422a4b3d1d456631c7773ecf24c929322
Daniel-Nashnaz <daniel.81953@gmail.com>
2024-11-01 02:24:02 +0200

    [INIT] initial project setup

---
+++ src/main/java/com/kosher/iskosher/IsKosherApplication.java
@@ -0,0 +1,13 @@
+package com.kosher.iskosher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsKosherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsKosherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosher/iskosher/IsKosherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosher.iskosher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsKosherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
