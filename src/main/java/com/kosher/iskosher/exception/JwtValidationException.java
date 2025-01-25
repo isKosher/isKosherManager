d0ca602f47543e8bfc5281d9aefcca3a1ee9946e
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-01-25 22:55:37 +0200

    [ADD] dto and custom exception

--- src/main/java/com/kosher/iskosher/exception/JwtValidationException.java
+++ src/main/java/com/kosher/iskosher/exception/JwtValidationException.java
@@ -1,7 +1,13 @@
 package com.kosher.iskosher.exception;
 
+import org.aspectj.bridge.Message;
+
 public class JwtValidationException extends RuntimeException {
     public JwtValidationException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public JwtValidationException(String message) {
+        super(message);
     }
 }
--- src/main/java/com/kosher/iskosher/types/LogoutStatus.java
+++
@@ -1,4 +0,0 @@
-package com.kosher.iskosher.types;
-
-public record LogoutStatus(boolean success, String message) {
-}
