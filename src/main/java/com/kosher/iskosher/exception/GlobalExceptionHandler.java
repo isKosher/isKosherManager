eb123e9bc5ad66d00ab9853ba311bf09dbef904a
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-01-26 02:09:42 +0200

    [MINOR] Remove import

--- src/main/java/com/kosher/iskosher/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kosher/iskosher/exception/GlobalExceptionHandler.java
@@ -11,12 +11,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
