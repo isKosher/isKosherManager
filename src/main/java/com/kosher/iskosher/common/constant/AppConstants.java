8125f653337989acfeccf9a45a4f360dbb122a93
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-02-09 21:54:46 +0200

    [SECURITY] Added CookieUtil for handling authentication cookies

--- src/main/java/com/kosher/iskosher/common/constant/AppConstants.java
+++ src/main/java/com/kosher/iskosher/common/constant/AppConstants.java
@@ -6,5 +6,11 @@ public class AppConstants {
     public static final int DURATION_CACHE_EXPIRE = 2;
     public static final TimeUnit TIME_UNIT_CACHE_EXPIRE = TimeUnit.HOURS;
     public static final long MAXIMUM_SIZE_CACHE = 1000;
+    public static final String ACCESS_TOKEN_COOKIE = "access_token";
+    public static final String REFRESH_TOKEN_COOKIE = "refresh_token";
+    public static final long ACCESS_TOKEN_DURATION = TimeUnit.HOURS.toSeconds(1);
+    public static final long REFRESH_TOKEN_DURATION = TimeUnit.DAYS.toSeconds(7);
+
+
 
 }
---
+++ src/main/java/com/kosher/iskosher/common/utils/CookieUtil.java
@@ -0,0 +1,83 @@
+package com.kosher.iskosher.common.utils;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+import static com.kosher.iskosher.common.constant.AppConstants.*;
+
+@Component
+public class CookieUtil {
+
+    public void createAccessTokenCookie(String token, HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from(ACCESS_TOKEN_COOKIE, token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(ACCESS_TOKEN_DURATION)
+                .sameSite("Strict")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public void createRefreshTokenCookie(String token, HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_COOKIE, token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(REFRESH_TOKEN_DURATION)
+                .sameSite("Strict")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public void deleteAccessTokenCookie(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from(ACCESS_TOKEN_COOKIE, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public void deleteRefreshTokenCookie(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_COOKIE, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public Optional<String> getAccessTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (ACCESS_TOKEN_COOKIE.equals(cookie.getName())) {
+                    return Optional.of(cookie.getValue());
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<String> getRefreshTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (REFRESH_TOKEN_COOKIE.equals(cookie.getName())) {
+                    return Optional.of(cookie.getValue());
+                }
+            }
+        }
+        return Optional.empty();
+    }
+}
