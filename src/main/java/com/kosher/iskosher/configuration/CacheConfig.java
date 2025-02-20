2f5cb850b10b9e702e08b7012baa4992926ab3a7
meire <meirelbaz32@gmail.com>
2025-02-20 02:48:53 +0200

    Added information on ways to get to businesses in the area

---
+++ src/main/java/com/kosher/iskosher/configuration/CacheConfig.java
@@ -0,0 +1,43 @@
+package com.kosher.iskosher.configuration;
+
+import org.ehcache.config.builders.*;
+import org.ehcache.jsr107.Eh107Configuration;
+import org.ehcache.jsr107.EhcacheCachingProvider;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public org.springframework.cache.CacheManager cacheManager() {
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        EhcacheCachingProvider ehcacheProvider = (EhcacheCachingProvider) cachingProvider;
+
+        CacheManager cacheManager = ehcacheProvider.getCacheManager(
+                ehcacheProvider.getDefaultURI(),
+                ehcacheProvider.getDefaultClassLoader()
+        );
+
+        cacheManager.createCache("travelInfoCache",
+                Eh107Configuration.fromEhcacheCacheConfiguration(
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                                        String.class, Object.class,
+                                        ResourcePoolsBuilder.heap(1000)
+                                )
+                                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(10)))
+                                .build()
+                )
+        );
+
+        return new JCacheCacheManager(cacheManager);
+    }
+}
