34bfd949b9d6e7cb2716a95bf10de3d686a23b0f
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-02-25 19:47:38 +0200

    [IMPROVE] Add list param to filter

--- src/main/java/com/kosher/iskosher/dto/request/BusinessFilterCriteria.java
+++ src/main/java/com/kosher/iskosher/dto/request/BusinessFilterCriteria.java
@@ -2,12 +2,14 @@ package com.kosher.iskosher.dto.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class BusinessFilterCriteria {
-    private String businessType;
-    private String foodType;
-    private String foodItemType;
+    private List<String> businessTypes;
+    private List<String> foodTypes;
+    private List<String> foodItemTypes;
     private String city;
-    private String kosherType;
+    private List<String> kosherTypes;
     private Integer rating;
 }
