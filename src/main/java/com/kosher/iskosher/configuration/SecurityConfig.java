795adae3eb35f613dc1911a5d1b08f5d01b884e6
Daniel-Nashnaz <daniel.81953@gmail.com>
2025-03-02 19:35:51 +0200

    [MINOR] Add  Cors config host

--- src/main/java/com/kosher/iskosher/configuration/SecurityConfig.java
+++ src/main/java/com/kosher/iskosher/configuration/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOriginPatterns(Arrays.asList(
-                "http://localhost:3000" // Allow local frontend
+                "http://localhost:3000", "http://127.0.0.1:3000" // Allow local frontend
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
